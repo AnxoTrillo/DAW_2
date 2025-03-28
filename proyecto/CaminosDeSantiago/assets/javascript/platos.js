49586c22107de827100429d52403ea166046f537
candedrums <rcandelab@gmail.com>
2025-03-28 19:01:53 +0100

    bootstrap

--- proyecto/CaminosDeSantiago/assets/javascript/platos.js
+++ proyecto/CaminosDeSantiago/assets/javascript/platos.js
@@ -1,30 +1,24 @@
 document.addEventListener("DOMContentLoaded", function () {
     let carousel = null;
-    if (screen.width > 567) {
+    if(screen.width > 567) {
         carousel = document.getElementById("carouselExample");
-    } else {
+
+    }
+    else{
         carousel = document.getElementById("carouselExampleSmallScreen");
     }
+    const prevButton = carousel.getElementsByTagName("button")[0];
+    const nextButton = carousel.getElementsByTagName("button")[1];
+    const items = carousel.querySelectorAll(".carousel-item");
 
-    if (carousel) {
-        const prevButton = carousel.getElementsByTagName("button")[0];
-        const nextButton = carousel.getElementsByTagName("button")[1];
-        const items = carousel.querySelectorAll(".carousel-item");
+    function updateButtons() {
+        const activeIndex = [...items].findIndex(item => item.classList.contains("active"));
 
-        function updateButtons() {
-            const activeIndex = [...items].findIndex(item => item.classList.contains("active"));
-            prevButton.style.display = activeIndex === 0 ? "none" : "block";
-            nextButton.style.display = activeIndex === items.length - 1 ? "none" : "block";
-        }
-
-        carousel.addEventListener("slid.bs.carousel", updateButtons);
-        updateButtons();
+        prevButton.style.display = activeIndex === 0 ? "none" : "block";
+        nextButton.style.display = activeIndex === items.length - 1 ? "none" : "block";
     }
 
-    window.onload = function () {
-        let link = document.createElement("link");
-        link.rel = "stylesheet";
-        link.href = "../../assets/styles/styles.css";
-        document.querySelectorAll("*")?.appendChild(link);
-    };
+    carousel.addEventListener("slid.bs.carousel", updateButtons);
+
+    updateButtons();
 });
