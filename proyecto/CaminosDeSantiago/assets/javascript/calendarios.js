e731bd42ed48047a4274de9bd51c44ea5ecaf8ab
AnxoTrillo <anxo.trillo@rai.usc.es>
2025-04-05 13:44:25 +0200

    calendario json

---
+++ proyecto/CaminosDeSantiago/assets/javascript/calendarios.js
@@ -0,0 +1,29 @@
+
+fetchjson()
+
+function fetchjson(){
+    fetch("../../assets/javascript/AJAX/fechas_calendario.json")
+        .then(r => r.json())
+        .then(jsonObj => verlista(jsonObj));
+}
+
+function verlista(jsonObj) {
+
+
+    let lis="";
+    let eventos=jsonObj.eventos
+
+    eventos.sort(function (a,b){
+        return new Date(a.fecha) - new Date(b.fecha)
+    });
+    let primerosEventos = eventos.slice(0, 4);
+
+    for (const evento of primerosEventos) {
+        let li=`<li class=\"${evento.tipo}\"><h2>${evento.nombre}</h2>`+
+            `<p>Fecha: ${evento.fecha}</p><p><a href=\"${evento.fecha}\">Visitar</a></p>`+
+    `</li>`;
+        lis+=li;
+    }
+
+    document.getElementById("calendar-container").innerHTML=`<ul>${lis}</ul>`;
+}
